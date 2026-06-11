2506e3b8ef013e86ed8f9760d0a5e3c01ca451ae
Ricardo Esper <resper@bekaa.eu>
2026-06-11 20:21:14 -0300

    feat(poam): implement dependency detection by shared SCF control and action type
    Co-Authored-By: Antigravity (Google DeepMind)

--- packages/poam/tests/run-tests.ts
+++ packages/poam/tests/run-tests.ts
@@ -3,6 +3,7 @@ import "./poam-prioritization.test";
 import "./poam-validation.test";
 import "./poam-approval.test";
 import "./tenancy.test";
+import "./poam-dependency.test";
 import { runTests } from "./test-kit";
 
 await runTests();
